cdfcf66c153e16b645f37b295d1b697e1b5fd5c7
Shaila Kavrakova <shaila.kavrakova@gmail.com>
2017-12-14 11:23:35 -0500

    set up devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:username, :email)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+  root to: "home#index"
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+  end
+
 end
