ba0e8e67e1fb0d6545135444519dc44f17345a68
Shaila Kavrakova <shaila.kavrakova@gmail.com>
2017-12-18 13:20:27 -0500

    added more neighborhoods to seed file + itinerary/new styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,12 @@
 
 
 wynwood = Neighborhood.find_or_create_by(name: "Wynwood")
+brickell = Neighborhood.find_or_create_by(name: "Brickell")
+south_beach = Neighborhood.find_or_create_by(name: "South Beach")
+downtown_miami = Neighborhood.find_or_create_by(name: "Downtown Miami")
+coconut_grove = Neighborhood.find_or_create_by(name: "Coconut Grove")
+little_havana = Neighborhood.find_or_create_by(name: "Little Havana")
+design_district = Neighborhood.find_or_create_by(name: "Design District")
+midtown_miami = Neighborhood.find_or_create_by(name: "Midtown Miami")
+little_haiti = Neighborhood.find_or_create_by(name: "Little Haiti")
+overtown = Neighborhood.find_or_create_by(name: "Overtown")
