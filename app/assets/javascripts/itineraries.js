2de4b2d4dac58be12e1c3646fdbc7db9349c1063
Kevin Schaffter <kevinschaffter@gmail.com>
2017-12-18 13:15:52 -0500

    Added title auto-fill and styled intro page

--- app/assets/javascripts/itineraries.js
+++ app/assets/javascripts/itineraries.js
@@ -87,6 +87,7 @@ function initMap(field) {
       var website = place.website
       var address = place.formatted_address
       var icon = place.icon
+      var title = place.name
 
 
       console.log(place)
@@ -126,6 +127,9 @@ function initMap(field) {
 
       $input.closest('.form-container').find("[name='itinerary[content][][website]']")
       .val(website)
+
+      $input.closest('.form-container').find("[name='itinerary[content][][title]']")
+      .val(title)
 
       if (!place.geometry) {
         // User entered the name of a Place that was not suggested and
