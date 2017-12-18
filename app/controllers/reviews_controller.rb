bcdf1260afbbf5c31b0cf6d12454d4645f172d3c
Steven <stevengabriel_@hotmail.com>
2017-12-17 23:21:50 -0500

    making reviews render on same page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,23 @@
 class ReviewsController < ApplicationController
   def index
     @reviewable = Itinerary.find(params[:itinerary_id])
-  
+
     @reviews = Review.all
   end
 
   def create
-    @review = Review.new
+    @review = Review.new(params[:review])
+    @review.save
   end
 
   def new
   end
+
+private
+
+def review_params
+  params.require(:review).permit(:name, :review, :itnerary_id)
+
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
+  get 'pages/home'
 
   get '/maze/:id', to: 'itineraries#intro', as: 'maze_intro'
 
   devise_for :users
-  
-  resources :users do      
+
+  resources :users do
     resource :reviews
   end
 
-  get 'pages/home'
-  
+
   resources :itineraries do
     resources :reviews
   end
 
-  resources :itineraries
-  
+  # resources :itineraries
+  post '/pages/home' => 'reviews#create'
 end
