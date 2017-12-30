2fcf6a597f684e341b4265bc1ab51ae61e965eb3
Shaila Kavrakova <shaila.kavrakova@gmail.com>
2017-12-30 14:51:44 -0500

    precompile fonts

--- config/application.rb
+++ config/application.rb
@@ -10,9 +10,10 @@ module Amaze
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.enabled = true  
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"  
-
+    config.assets.enabled = true
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
